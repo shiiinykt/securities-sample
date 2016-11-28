2d69d8b8227f67028bdb704643937cc4aa321bc1
shiiinykt <shiiinykt@sample.com>
2016-11-29 00:47:29 +0900

    Modified AbstractDao

--- src/main/java/com/heroku/shiiinykt_securiteis_sample/AbstractDao.java
+++ src/main/java/com/heroku/shiiinykt_securiteis_sample/AbstractDao.java
@@ -7,7 +7,7 @@ public class AbstractDao {
 	
 	protected Db db;
 	
-	private static String DB_URL = "jdbc:mysql://localhost:3306/securities_sample?useUnicode=yes&characterEncoding=UTF-8";
+	private static String DB_URL = "jdbc:mysql://" + System.getenv("DB_URL") + ":3306/" + System.getenv("DB_DATABASE")  + "?useUnicode=yes&characterEncoding=UTF-8";
 	private static String DB_USER = System.getenv("DB_USER");
 	private static String DB_PASSWORD = System.getenv("DB_PASSWORD");
 	
